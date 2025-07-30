2a8dd730f20ae1c24f657acc686c91ed481b5e57
Victor Guzmán Berrio <156972328+solaradevelopment@users.noreply.github.com>
2025-07-30 17:07:47 -0500

    Remove sensitive API keys and add environment setup documentation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
-  googlePlacesApiKey: 'AIzaSyAnIgjBzvSbCBUmrla6Sa6R96hqkdkZSck',
-  glowSkinPlaceId: '0x8ef42d0017f3d5ed:0x9c66106c69176e22',
+  googlePlacesApiKey: 'YOUR_GOOGLE_PLACES_API_KEY',
+  glowSkinPlaceId: 'YOUR_GLOW_SKIN_PLACE_ID',
   googlePlacesApiUrl: 'https://maps.googleapis.com/maps/api/place',
 
   googleOauth: {
-    clientId:
-      '818014543480-69c5hdbbo140jpcvv94oq0tf74s0v471.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientId: 'YOUR_GOOGLE_OAUTH_CLIENT_ID',
+    clientSecret: 'YOUR_GOOGLE_OAUTH_CLIENT_SECRET',
   },
 
   whatsappLink: 'https://wa.link/h5481r',
